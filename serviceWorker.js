4f00041e03db2b3935f18a3d064227093c199d3f
Masato-Nasu <1bitexist@gmail.com>
2025-11-02 11:17:05 +0900

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,5 @@
-const CACHE='dictshift-pwa-v2';
-const ASSETS=['./','./index.html','./style.css','./script.js','./manifest.json','./assets/icon-192.png','./assets/icon-512.png','./assets/dict.txt'];
+const CACHE='oulipo-s7-pwa-v1';
+const ASSETS=['./','./index.html','./style.css','./script.js','./manifest.json','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();})());});
 self.addEventListener('fetch',e=>{const url=new URL(e.request.url);if(url.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));}});
